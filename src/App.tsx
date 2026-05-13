b21ef8a4160f6586b8849246af45817a00113aa5
adane-desta <addonegn@gmail.com>
2026-05-13 23:07:20 +0300

    payment gateway

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider } from 'react-router';
+import { router } from './routes';
+import { AuthProvider } from './contexts/AuthContext';
+import { Toaster } from './components/ui/sonner';
+
+//fRAnjhTNDnmu
+export default function App() {
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <Toaster position="top-right" richColors closeButton />
+    </AuthProvider>
+  );
+}
